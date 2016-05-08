15898a605022f767e5ec9ed30549a46a72d64ed0
ajlopez <webmaster@ajlopez.com>
2016-05-08 09:09:57 -0300

    Create genesis block

---
+++ test/blocks.js
@@ -0,0 +1,41 @@
+
+var blocks = require('../lib/blocks');
+
+function isHexadecimal(text) {
+    for (var k = 0; k < text.length; k++) {
+        var ch = text[k];
+        if (ch >= '0' && ch <='9')
+            continue;
+        if (ch >= 'a' && ch <= 'f')
+            continue;
+            
+        return false;
+    }
+    
+    return true;
+}
+
+function isHash(hash) {
+    if (typeof hash !== 'string')
+        return false;
+        
+    if (hash.length != 66)
+        return false;
+        
+    if (hash.substring(0, 2) !== '0x')
+        return false;
+        
+    return isHexadecimal(hash.substring(2));
+}
+
+exports['create genesis block'] = function (test) {
+    var block = blocks.block();
+    
+    test.ok(block);
+    test.equal(typeof block, 'object');
+    test.equal(block.number, 0);
+    test.ok(isHash(block.hash));
+    test.ok(isHash(block.parentHash));
+    test.equal(block.parentHash, '0x0000000000000000000000000000000000000000000000000000000000000000');
+}
+

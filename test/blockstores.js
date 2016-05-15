7c8d3672202a42d210ef2bf3bbac55af548b1b84
ajlopez <webmaster@ajlopez.com>
2016-05-15 09:48:43 -0300

    Block store save block by hash

--- test/blockstores.js
+++ test/blockstores.js
@@ -17,4 +17,16 @@ exports['retrieve unknown block by hash'] = function (test) {
     test.equal(block, null);
 };
 
+exports['save block and retrieve it by hash'] = function (test) {
+    var store = stores.blockstore();
+    var hash = utils.hash();
+    var block = { hash: hash };
+    
+    store.save(block);
+    
+    var result = store.getByHash(hash);
+    
+    test.ok(result);
+    test.equal(result.hash, hash);
+};
 

6ec5d5742721475222fff3ba0b2885ec1f83f8bd
ajlopez <webmaster@ajlopez.com>
2016-05-16 08:43:47 -0300

    Block store by parent hash

--- test/blockstores.js
+++ test/blockstores.js
@@ -17,6 +17,16 @@ exports['retrieve unknown block by hash'] = function (test) {
     test.equal(block, null);
 };
 
+exports['retrieve unknown block by parent hash'] = function (test) {
+    var store = stores.blockstore();
+    
+    var result = store.getByParentHash(utils.hash());
+    
+    test.ok(result);
+    test.ok(Array.isArray(result));
+    test.equal(result.length, 0);
+};
+
 exports['save block and retrieve it by hash'] = function (test) {
     var store = stores.blockstore();
     var hash = utils.hash();
@@ -28,5 +38,22 @@ exports['save block and retrieve it by hash'] = function (test) {
     
     test.ok(result);
     test.equal(result.hash, hash);
+};
+
+exports['save block and retrieve it by parent hash'] = function (test) {
+    var store = stores.blockstore();
+    var hash = utils.hash();
+    var parentHash = utils.hash();
+    var block = { hash: hash, parentHash: parentHash };
+    
+    store.save(block);
+    
+    var result = store.getByParentHash(parentHash);
+    
+    test.ok(result);
+    test.ok(Array.isArray(result));
+    test.equal(result.length, 1);
+    test.equal(result[0].hash, hash);
+    test.equal(result[0].parentHash, parentHash);
 };
 

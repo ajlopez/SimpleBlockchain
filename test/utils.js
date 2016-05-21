0c012d1d07c0bc7d799e8e455d042b2d77859a44
ajlopez <webmaster@ajlopez.com>
2016-05-21 07:19:06 -0300

    Utils isHexadecimal

--- test/utils.js
+++ test/utils.js
@@ -1,28 +1,13 @@
 
 var utils = require('../lib/utils');
 
-function isHexadecimal(text) {
-    for (var k = 0; k < text.length; k++) {
-        var ch = text[k];
-        if (ch >= '0' && ch <='9')
-            continue;
-        if (ch >= 'a' && ch <= 'f')
-            continue;
-            
-        return false;
-    }
-    
-    return true;
-}
-
 exports['generate hash'] = function (test) {
     var hash = utils.hash();
     
     test.ok(hash);
     test.equal(typeof hash, 'string');
-    test.equal(hash.substring(0, 2), '0x');
     test.equal(hash.length, 66);
-    test.ok(isHexadecimal(hash.substring(2)));
+    test.ok(utils.isHexadecimal(hash));
     test
 };
 
@@ -31,9 +16,8 @@ exports['generate address'] = function (test) {
     
     test.ok(hash);
     test.equal(typeof hash, 'string');
-    test.equal(hash.substring(0, 2), '0x');
     test.equal(hash.length, 42);
-    test.ok(isHexadecimal(hash.substring(2)));
+    test.ok(utils.isHexadecimal(hash));
     test
 };
 

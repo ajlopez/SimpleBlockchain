ba210c0e046f10d0ba090d600d1c290180df5978
ajlopez <webmaster@ajlopez.com>
2016-06-01 07:11:42 -0300

    Trie additional test

--- test/tries.js
+++ test/tries.js
@@ -34,4 +34,16 @@ exports['put data and get data'] = function (test) {
     test.equal(result, 42);
 };
 
+exports['put two new data and retrieve them'] = function (test) {
+    var trie = tries.trie();
+    
+    var result = trie.put('0123', 42)
+        .put('3210', 1);
+    
+    test.ok(result);
+    test.equal(result.get('0123'), 42);
+    test.equal(result.get('3210'), 1);
+    test.equal(trie.get('0123'), null);
+    test.equal(trie.get('3210'), null);
+};
 

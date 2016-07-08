2d4e170f8a6454e0ad492eb0ce34ddf461a00c93
ajlopez <webmaster@ajlopez.com>
2016-07-08 16:06:24 -0300

    Additional test, change account balance in states

--- test/states.js
+++ test/states.js
@@ -14,3 +14,26 @@ exports['create states with default value'] = function (test) {
     test.equal(result.balance, 0);
 };
 
+exports['change balance'] = function (test) {
+    var states = tries.states();
+    
+    test.ok(states);
+    test.equal(typeof states, 'object');
+    
+    var newstates = states.put('1234', { balance: 1000 });
+
+    test.ok(newstates);
+    
+    var result = states.get('1234');
+    
+    test.ok(result);
+    test.equal(typeof result, 'object');
+    test.equal(result.balance, 0);
+    
+    var result = newstates.get('1234');
+    
+    test.ok(result);
+    test.equal(typeof result, 'object');
+    test.equal(result.balance, 1000);
+};
+

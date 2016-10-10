47a9ed8054df25e839a31d2781756efe71c5d777
ajlopez <webmaster@ajlopez.com>
2016-10-10 12:42:54 -0300

    Node add block

--- test/nodes.js
+++ test/nodes.js
@@ -1,10 +1,24 @@
 
 var nodes = require('../lib/nodes');
+var blocks = require('../lib/blocks');
 
 exports['create node'] = function (test) {
 	var node = nodes.node();
 	
 	test.ok(node);
 	test.equal(typeof node, 'object');
+};
+
+exports['add genesis block'] = function (test) {
+    var genesis = blocks.block();
+	var node = nodes.node();
+	
+	node.addBlock(genesis);
+	
+	var best = node.bestBlock();
+	
+	test.ok(best);
+	test.equal(best.number, genesis.number);
+	test.equal(best.hash, genesis.hash);
 };
 

d8850423878fdcafdf5e2bbe7a4efc483cccacb6
ajlopez <webmaster@ajlopez.com>
2017-02-19 14:31:13 -0300

    Transactions in node

--- test/nodes.js
+++ test/nodes.js
@@ -1,6 +1,8 @@
 
 var nodes = require('../lib/nodes');
 var blocks = require('../lib/blocks');
+var transactions = require('../lib/transactions');
+var utils = require('../lib/utils');
 
 exports['create node'] = function (test) {
 	var node = nodes.node();
@@ -20,5 +22,30 @@ exports['add genesis block'] = function (test) {
 	test.ok(best);
 	test.equal(best.number, genesis.number);
 	test.equal(best.hash, genesis.hash);
+};
+
+exports['add transfer'] = function (test) {
+    var from = utils.hash();
+    var to = utils.hash();
+    var value = 1000;
+    
+    var tx = transactions.transfer(from, to, value);
+	
+	var node = nodes.node();
+	
+	node.addTransaction(tx);
+
+	var txs = node.transactions();
+	
+	test.ok(txs);
+	test.ok(Array.isArray(txs));
+	test.equal(txs.length, 1);
+	
+	var ntx = txs[0];
+	
+	test.equal(ntx.id, tx.id);
+	test.equal(ntx.to, tx.to);
+	test.equal(ntx.value, tx.value);
+	test.equal(ntx.from, tx.from);
 };
 
